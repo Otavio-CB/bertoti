e9b0ce8833c5f71af32d01e85f279fb4a12577e8
ocalderan <otaviocalderanadm@gmail.com>
2024-11-11 17:31:48 -0300

    ♻️ (refactor): Refactor singleton

--- design_patterns/singletonPattern/src/main/java/org/rest/SingletonPatternDemo.java
+++ design_patterns/singletonPattern/src/main/java/org/rest/SingletonPatternDemo.java
@@ -1,13 +1,10 @@
 package org.rest;
 
-// Cliente
 public class SingletonPatternDemo {
     public static void main(String[] args) {
 
-        // Obter a única instância do Singleton
         Singleton singleton = Singleton.getInstance();
 
-        // Usar o Singleton
         singleton.showMessage();
     }
 }
